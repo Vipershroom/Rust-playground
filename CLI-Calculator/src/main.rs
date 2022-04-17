393d8283ddebbbe37ddf542ec43f9aa1004463ff
Vipershroom <undertalefan812@gmail.com>
2022-04-17 18:34:32 -0400

    Added ability to exit

--- CLI-Calculator/src/main.rs
+++ CLI-Calculator/src/main.rs
@@ -6,23 +6,24 @@ mod global;
 
 fn display_ui_1() {
         loop {
-        println!("
->Addition(add)
+        println!(">Addition(add)
 >Subtraction(subtract)
 >Multiplication(multi)
 >Division(divide)
->Interactive mode(Not finished)
 >Next
+>Interactive mode(Not finished)
 >Options
 >Exit"
 );
         let request = input();
-        match request.as_str() {
+        match request.to_lowercase().as_str() {
             "add" => arithmatic::addition(),
             // "subtract" => arithmatic::subtraction(),
             // "multi" => arithmatic::multiply(),
             // "divide" => arithmatic::divide(),
             // "next" => arithmatic::next(),
+            "exit" => {println!("Exiting....");
+                      return}
             _ => println!("Please input a valid option")
         }
     };
