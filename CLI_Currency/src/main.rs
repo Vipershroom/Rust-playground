4c1d23381e1519ae8547afbb8ac6d7d7e4ecd710
Vipershroom <undertalefan812@gmail.com>
2022-04-22 19:16:35 -0400

    Added ability to dynamically change language

--- CLI_Currency/src/main.rs
+++ CLI_Currency/src/main.rs
@@ -1,29 +1,49 @@
+
+
+use std::{collections::HashMap};
+
 use crate::global::input;
 
 mod global;
 mod currencys;
 
-fn default_currency() {
-    println!("Please enter your default currency")
+fn default_currency() -> String{
+    loop {
+        println!("Please enter your default currency");
+        let m = vec!["USD", "Yen", "Ruble", "Euro", "Real"];
+        for i in &m {
+            println!(">{i}");
+        }
+        let cur = input();
+        match cur.to_lowercase().as_str() {
+            "usd" => return m[0].to_string(),
+            "yen" => return m[1].to_string(),
+            "ruble" => return m[2].to_string(),
+            "euro" => return m[3].to_string(),
+            "real" => return m[4].to_string(),
+            _ => {
+                println!("\nPlease enter a valid currency\n");
+                continue
+            }
+        }
+    }
 }
 
 fn display_screen() {
-    let mut default = "USD";
-    let m = ["USD", "Japanese", "Russian", "EU", "Brazillian"];
+    let mut default = default_currency();
+    let langlist = vec!["USD", "Yen", "Ruble", "Euro", "Real"];
     loop {
-        println!("
-Default: {}
->Japanese(Yen)
->Russian(Ruble)
->EU(Euro)
->Brazilian (Real)
->Options
->Exit", default);
+        println!("\nYour default language is {}", default);
+        for i in &langlist {
+            if i.to_lowercase() != default.to_lowercase() {
+                println!(">{}", i)
+            } 
+        }
 
         let m = input();
         match m.to_lowercase().as_str() {
             "yen" => currencys::yen(),
-            _ => println!("Please enter a valid value")
+            _ => println!("\n Please enter a valid value")
         }
     }
 }
