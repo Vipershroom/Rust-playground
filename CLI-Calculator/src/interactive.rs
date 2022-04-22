03c7f9dc23b4ad98d83a612f011b3810d2a0b86e
Vipershroom <undertalefan812@gmail.com>
2022-04-22 09:51:40 -0400

    cleaning branch

---
+++ CLI-Calculator/src/interactive.rs
@@ -0,0 +1,9 @@
+use crate::global::{input, int_input};
+
+pub fn Interactive() {
+    println!("Please Enter your equation");
+    let equation = input();
+    for i in equation.chars() {
+        
+    }
+}
--- Handbook/src/main.rs
+++ Handbook/src/main.rs
@@ -1,6 +1,3 @@
 fn main() {
-    let mut data = String::from("foo");
-    let bar = "bar"
-    data.push(bar);
-    println!("{data} {bar}")
+    
 }
