c5309cab3ad4e7c02ea500b5b4a4a560dbd89927
Vipershroom <undertalefan812@gmail.com>
2022-04-23 19:10:16 -0400

    Added conversions to real.rs

--- CLI_Currency/src/currencys/real.rs
+++ CLI_Currency/src/currencys/real.rs
@@ -11,17 +11,61 @@ pub fn real(lang: &str) {
 }
 
 fn usd_to_real() {
-
+    loop {
+        println!("Please input your usd amount");
+        let inp = num_input();
+        let result = inp * 4.80;
+        println!("${} USD is equal to R${:.2} real", inp, result);
+        println!("Would you like to go again? (Y/n)");
+            let yn = input();
+            match yn.to_lowercase().as_str() {
+                "y" => continue,
+                _ => break
+        }
+    }
 }
 
 fn yen_to_real() {
-
+    loop {
+        println!("Please input your yen amount");
+        let inp = num_input();
+        let result = inp * 0.037;
+        println!("¥{} yen is equal to R${:.2} real", inp, result);
+        println!("Would you like to go again? (Y/n)");
+            let yn = input();
+            match yn.to_lowercase().as_str() {
+                "y" => continue,
+                _ => break
+        }
+    }
 }
 
 fn ruble_to_real() {
-
+    loop {
+        println!("Please input your ruble amount");
+        let inp = num_input();
+        let result = inp * 0.062;
+        println!("₽{} ruble is equal to R${:.2} real", inp, result);
+        println!("Would you like to go again? (Y/n)");
+            let yn = input();
+            match yn.to_lowercase().as_str() {
+                "y" => continue,
+                _ => break
+        }
+    }
 }
 
 fn euro_to_real() {
-    
+    loop {
+        println!("Please input your euro amount");
+        let inp = num_input();
+        let result = inp * 5.19;
+        println!("€{} euro is equal to R${:.2} real", inp, result);
+        println!("Would you like to go again? (Y/n)");
+            let yn = input();
+            match yn.to_lowercase().as_str() {
+                "y" => continue,
+                _ => break
+        }
+    }
 }
