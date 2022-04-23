54f63151942075966e9ba05da8750ed24b2d3b02
Vipershroom <undertalefan812@gmail.com>
2022-04-23 00:18:28 -0400

    Added ruble to usd

--- CLI_Currency/src/currencys/USD.rs
+++ CLI_Currency/src/currencys/USD.rs
@@ -4,7 +4,7 @@ pub fn usd(lang: &str) {
     let curLang = vec!["Yen", "Ruble", "Euro", "Real"];
     match lang {
         "yen" => yen_to_usd(),
-        // "ruble" => ruble_to_usd(),
+        "ruble" => ruble_to_usd(),
         // "euro" => euro_to_usd(),
         // "real" => real_to_usd(),
         _ => println!("This should never hit")
@@ -16,4 +16,11 @@ fn yen_to_usd() {
     let inp = num_input();
     let result = inp * 0.0078;
     println!("{} yen is equal to ${:.2} USD",inp, result)
+}
+
+fn ruble_to_usd() {
+    println!("Please input your ruble amount");
+    let inp = num_input();
+    let result = inp * 0.013;
+    println!("{} ruble is equal to ${:.2} USD", inp, result)
 }
