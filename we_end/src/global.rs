12da7833b343540b7e1dd22fb5b35f9c80ed7a6f
Lucas <undertalefan812@gmail.com>
2022-04-25 12:30:31 -0400

    Added we_end project

---
+++ we_end/src/global.rs
@@ -0,0 +1,9 @@
+fn input() -> String {
+    let buff = String::new();
+
+    std::io::stdin().read_line(&mut buff).unwrap();
+
+    match buff {
+        
+    }
+}
---
+++ we_end/src/main.rs
@@ -0,0 +1,8 @@
+use std::fs::File;
+use serde_json::Json;
+
+fn main() {
+    println!("Welcome to my H Drive maker");
+    println!("This is your H:Drive correct? (Y/n)");
+    println!("\"H:\\2021_2022\"")
+}
