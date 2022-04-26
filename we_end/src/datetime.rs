631ff6909d464df24d5993001f8aacfb74baf162
Lucas <undertalefan812@gmail.com>
2022-04-26 11:06:07 -0400

    add datetime and file

---
+++ we_end/src/datetime.rs
@@ -0,0 +1,9 @@
+use chrono;
+
+struct CurrentDay {
+    monday: u32,
+    tuesday: u32,
+    wednesday: u32,
+    thursday: u32,
+    friday: u32,
+}
--- we_end/src/main.rs
+++ we_end/src/main.rs
@@ -1,5 +1,3 @@
-
-
 mod handle_json;
 mod global;
 
