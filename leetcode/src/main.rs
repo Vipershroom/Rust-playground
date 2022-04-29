cc1c612154b9df02c08076f6a529ba26d7b27624
Lucas <undertalefan812@gmail.com>
2022-04-29 14:13:15 -0400

    haha SOLUTION

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,3 +1,16 @@
+
+
+fn vec_to_target(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    for (i, elem) in nums.iter().enumerate() {
+        for (k,elem2) in nums.iter().enumerate() {
+            if elem + elem2 == target{
+                return vec![i as i32,k as i32]
+            }
+        }
+    }
+    unreachable!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", vec_to_target(vec![9,6,6,2], 6));
 }
