59e470cbf1dde9b74e60cc1f9a3aade26eda57c9
Vipershroom <undertalefan812@gmail.com>
2022-06-21 15:35:35 -0400

    Read from json

--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -1,15 +1,8 @@
 use hangman::input;
-use std::{fs::File, io::Read};
+use std::{fs::{File, self}, io::Read};
 use serde_json::{self, Value};
 
 fn get_words(diff: String) {
-    let mut f = File::open("words.json").unwrap_or_else(|error| {
-        panic!("Error Reading words.json")
-    });
-    let mut json_contents = String::new();
-    f.read_to_string(&mut json_contents).expect("Failed to read file");
-    let contents = serde_json::from_str::<Value>(&json_contents).unwrap();
-    
 
     let diff = match diff.to_lowercase().trim(){
         "e" => "Easy",
@@ -18,8 +11,21 @@ fn get_words(diff: String) {
         _ => panic!("Select a difficulty")
     };
 
-    let dir_string = contents["Easy"].as_str().unwrap();
-    println!()
+    let mut f = File::open("words.json").unwrap_or_else(|error| {
+        panic!("Error Reading words.json")
+    });
+    
+    let contents = fs::read_to_string("words.json").unwrap_or_else(|error|{
+        panic!("Error reading file")
+    });
+
+    let contents = serde_json::from_str::<Value>(&contents).unwrap();
+
+    let length_of_word_list = &contents[diff]["Words"].to_string().len();
+
+    println!("{}", &contents[diff]["Words"].to_string());
+
+    println!("{}", length_of_word_list)
 
 }
 
