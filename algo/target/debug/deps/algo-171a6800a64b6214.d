562839474b75815628cc220825915e566a35d80c
Vipershroom <undertalefan812@gmail.com>
2022-07-01 16:07:38 -0400

    bye 2

--- algo/target/debug/deps/algo-171a6800a64b6214.d
+++
@@ -1,6 +0,0 @@
-c:\Users\Lucas\Desktop\Rust playground\algo\target\debug\deps\algo-171a6800a64b6214.rmeta: src\lib.rs src\my_search.rs
-
-c:\Users\Lucas\Desktop\Rust playground\algo\target\debug\deps\algo-171a6800a64b6214.d: src\lib.rs src\my_search.rs
-
-src\lib.rs:
-src\my_search.rs:
--- maths/target/debug/deps/maths-83e193651b53a993.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Lucas\Desktop\Rust playground\maths\target\debug\deps\maths-83e193651b53a993.exe: src\lib.rs src\sum.rs
-
-C:\Users\Lucas\Desktop\Rust playground\maths\target\debug\deps\maths-83e193651b53a993.d: src\lib.rs src\sum.rs
-
-src\lib.rs:
-src\sum.rs:
